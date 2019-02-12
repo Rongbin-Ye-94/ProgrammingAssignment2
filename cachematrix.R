f7741e86817a28f814173e5e16b57ae7002035f6
RBYe94 <rongbin.ye.94@gmail.com>
2019-02-12 01:08:29 -0500

    aster

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special “matrix” object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  hidden <- NULL
+  set <- function(y){
+    x <<- y
+    hidden <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) hidden <<- solveMatrix
+  getInverse <- function() hidden
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## The function calculates the Inverse of the special “matrix” created with the above function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+  hidden <- x$getInverse()
+  if(!is.null(hidden)) {
+    message("getting cached data")
+    return(hidden)
+  }
+  data <- x$get()
+  hidden <- solve(data, ...)
+  x$setInverse(hidden)
+  hidden
 }
